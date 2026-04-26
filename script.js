d7d309e6096e0044b8877babfdfac9f31c85f28e
lunahazel200512-dotcom <lunahazel200512@gmail.com>
2026-04-26 09:08:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ const movies = [
         description: "After a great catastrophe, the souls of Nezha and Aobing are saved, but their bodies face ruin. To give them new life, Taiyi Zhenren turns to the mystical seven-colored lotus in a daring bid to rebuild them and change their fate. The highest-grossing animated film of all time with $2.26 billion worldwide.",
         imdbId: "tt38693888",
         server1: "https://vidsrc.xyz/embed/movie/tt38693888",
-        server2: "https://otieu.com/4/9200996",
+        server2: "https://vsembed.ru/embed/movie?imdb=tt38693888/",
         telegramLink: "https://t.me/your_link",
         showPlayer: true,
         showTelegram: true,
